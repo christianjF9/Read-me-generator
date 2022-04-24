f30fe72e42c42046f0dc147acb3d068acc197d82
Christian Foote <christianfoote999@gmail.com>
2022-04-24 03:44:02 -0400

    change file structure

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,7 @@ function renderLicenseBadge(license) {
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+// If there is no license, return an empty string 
 function renderLicenseLink(license) {
   return license.license;
 }
